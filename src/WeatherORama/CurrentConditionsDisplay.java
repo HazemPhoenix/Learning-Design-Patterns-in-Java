00cad38af2d4e2ab9e10cde193ca81e865a86334
hazemphoenix <hazem.mando408@gmail.com>
2025-06-16 20:22:48 +0300

    feat: implement the CurrentConditionsDisplay observer class

--- src/WeatherORama/CurrentConditionsDisplay.java
+++ src/WeatherORama/CurrentConditionsDisplay.java
@@ -1,9 +1,19 @@
 package WeatherORama;
 
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private float pressure;
 
 
-    public void update() {
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
 
+    public void display() {
+        System.out.println("Current Conditions: " + temperature + "degree Celsius, and " + humidity + "%");
     }
 }
